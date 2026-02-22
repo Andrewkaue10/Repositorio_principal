d50934738a0988165b0834a046d6c23224bb8138
Andrew Kauê <andrewkaue13@gmail.com>
2026-02-22 20:12:34 +0000

    introdução a java

---
+++ java.b/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Hello word");
+    }
+}
+
+int idade = 18;
+double altura = 1.75;
+char letra = 'A';
+boolean ativo = true;
+String nome = "Andrew";
+
+int a = 10
+int b = 5
+
+System.out.println(a + b); //Adição 
+System.out.println(a - b); //Subtração
+System.out.println(a * b); //Multiplicação
+System.out.println(a / b); //Divisão
+
+if (idade >= 18){
+    Systeam.out.println("Maior de idade");
+   } else { 
+    Systeam.out.println("Menor de idade");
+}
+
+
+
